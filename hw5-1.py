063d7be5b2d628625c3fd6b914cb8d0812973a08
p22dhealy <p22dhealy@fairfieldprep.org>
2021-11-03 00:07:21 -0400

    submit hw 5-1

---
+++ hw5-1.py
@@ -0,0 +1,12 @@
+#author: DMH 11/2/21
+
+import random 
+
+#question 1
+print(random.randint(31 ,49))
+
+#question 2
+print(random.randrange(2, 75, 2))
+
+#question 3
+print(random.randrange(21,31,2))
